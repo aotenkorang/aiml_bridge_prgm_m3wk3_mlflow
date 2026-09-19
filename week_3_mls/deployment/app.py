59951ad7dca54c35884dd7faf8dedc0e8c5f5d67
O. Tenkorang A. <aotenkorang@gmail.com>
2026-09-19 03:30:14 +0000

    Update app.py

--- week_3_mls/deployment/app.py
+++ week_3_mls/deployment/app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_icon="⚙️"
     )
 st.markdown(
-    "<h1 style='font-size: 36px;'>AI Powered Machine Failure Prediction App</h1>",
+    "<h1 style='font-size: 36px;'>AI-Powered Machine Failure Prediction App</h1>",
     unsafe_allow_html=True
 )
 # st.title("AI Powered Machine Failure Prediction App")
