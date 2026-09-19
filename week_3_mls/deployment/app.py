8b2818579fa40a6e45395d6c3f07c4ed30fc128a
O. Tenkorang A. <aotenkorang@gmail.com>
2026-09-19 03:11:56 +0000

    Update app.py

--- week_3_mls/deployment/app.py
+++ week_3_mls/deployment/app.py
@@ -8,9 +8,10 @@ model_path = os.path.join(os.path.dirname(__file__), "best_machine_failure_model
 model = joblib.load(model_path)
 st.set_page_config(
     page_title="AI Machine Failure Prediction System",
-    page_icon="⚙️"
+    page_icon="⚙️",
+    layout="wide"
 )
-st.title("Machine Failure Prediction App System")
+st.title("AI Powered Machine Failure Prediction App")
 st.write("""
 This application predicts the likelihood of a machine failing based on its operational parameters.
 Enter the sensor and configuration data below to get a prediction.
